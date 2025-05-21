5ca86fd8f99930e7295f7201c6bf0cf00ec99596
Becky White <white.rebecca2012@gmail.com>
2025-05-21 15:44:24 +0100

    Secure down main.tf by obscuring variables

--- tf-create-basic-ec2/main.tf
+++ tf-create-basic-ec2/main.tf
@@ -3,7 +3,7 @@
 # where to create - provide cloud provider name
 provider "aws" {
     # which region to create the resource/service
-  region = "eu-west-1"
+  region = var.region
 }
 
 # which resource to create, 
@@ -11,19 +11,16 @@ provider "aws" {
 # "app_instance" is the referal name that terraform will use
 resource "aws_instance" "app_instance" {
     # which AMI ID to use (ami-0c1c30571d2dae5c9) for Ubuntu 22.04 LTS
-    ami = "ami-0c1c30571d2dae5c9"
+    ami = var.app-ami-id
 
     # which instance type t3.micro
-    instance_type = "t3.micro"
+    instance_type = var.instance-type
 
     # add a public IP address to this instance 
-    associate_public_ip_address = true
+    associate_public_ip_address = var.associate_public_ip_address
 
     # name the instance
     tags = {
-        Name = "tech504-becky-terraform-test-app" # AWS will call the instance this name tag
+        Name = var.instance-name # AWS will call the instance this name tag
     }
 }
-
-# keep in mind the syntax for HCL Language, key value pairs
-
